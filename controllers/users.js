b0e8bce93993c53310f2399594b8976d75ce15d0
lukakhuza <lukakhuza7@gmail.com>
2025-05-20 05:17:23 -0400

    minor update

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,7 @@
 const User = require("../models/user");
 
 exports.createUser = (req, res, next) => {
-  console.log("Here is the request body", req.params);
+  console.log("Here is the request body", req.body.email);
   const firstName = "Luka";
   const lastName = "Khuz";
   const email = "LukaTest@gmail.com";
