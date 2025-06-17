59bee412bd69d9b866d6ad34490a0d6cb6cab34d
lukakhuza <lukakhuza7@gmail.com>
2025-06-17 09:21:48 -0400

    test commit 49

--- controllers/products.js
+++ controllers/products.js
@@ -97,7 +97,7 @@ exports.postCart = (req, res, next) => {
   const updatedCart = product;
   const updatedShopFor = "Men";
   console.log("Test 55", updatedCart);
-  User.findOne({ email: req.body.email }).then((user) => {
+  User.findOne({ email: user.email }).then((user) => {
     console.log("Test 56", user);
     //     user.firstName = updatedFirstName;
     //     user.lastName = updatedLastName;
