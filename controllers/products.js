800b000d20fe50a1e7d887df2e95c085779cc1f7
lukakhuza <lukakhuza7@gmail.com>
2025-06-17 09:25:50 -0400

    test commit 50

--- controllers/products.js
+++ controllers/products.js
@@ -97,22 +97,23 @@ exports.postCart = (req, res, next) => {
   const updatedCart = product;
   const updatedShopFor = "Men";
   console.log("Test 55", updatedCart);
-  User.findOne({ email: user.email }).then((user) => {
-    console.log("Test 56", user);
-    //     user.firstName = updatedFirstName;
-    //     user.lastName = updatedLastName;
-    //     user.email = updatedEmail;
-    //     user.phoneNumber = updatedPhoneNumber;
-    //     user.address = updatedAddress;
-    //     user.shopFor = updatedShopFor;
-    //     return user.save();
-  });
-  //   .then((result) => {
-  //     console.log("Updated User!");
-  //   })
-  //   .catch((err) => {
-  //     console.log(err);
-  //   });
+  User.findOne({ email: user.email })
+    .then((user) => {
+      user.firstName = updatedFirstName;
+      user.lastName = updatedLastName;
+      user.email = updatedEmail;
+      user.phoneNumber = updatedPhoneNumber;
+      user.address = updatedAddress;
+      user.shopFor = updatedShopFor;
+      user.cart.items = product;
+      return user.save();
+    })
+    .then((result) => {
+      console.log("Updated User!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 
   // req.user.addProductToCart(product);
   // const prodId = req.body.productId;
